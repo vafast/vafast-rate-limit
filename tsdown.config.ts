6fbb59e07344141bf3be7da1f4597f4feea2763f
付国强 <434543162@qq.com>
2026-01-07 18:16:40 +0800

    chore: migrate from tsup to tsdown

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm'],
   dts: true,
-  sourcemap: true,
   clean: true,
-  external: ['elysia', 'debug', '@alloc/quick-lru'],
+  sourcemap: true,
+  outDir: 'dist',
 })
